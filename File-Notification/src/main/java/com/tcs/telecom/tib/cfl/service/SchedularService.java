0b66905f4f0e6e5a929d429f8a1529e388dc4f4e
ShashankKedar <ShashankKedar@10.2.0.43>
2022-10-06 14:14:42 +0530

    schedular time chages 2

--- File-Notification/src/main/java/com/tcs/telecom/tib/cfl/service/SchedularService.java
+++ File-Notification/src/main/java/com/tcs/telecom/tib/cfl/service/SchedularService.java
@@ -54,9 +54,9 @@ public class SchedularService {
 	@Autowired
 	SOAP_Service soapService;
 	
-	   @Scheduled(fixedRate = 60000)
+	   //@Scheduled(fixedRate = 60000)
 	  // @Scheduled(cron = "0 0 21 * * MON-FRI",zone = "BST")
-	   //@Scheduled(cron = "0 0/15 7-19 * * MON-FRI",zone = "BST")
+	   @Scheduled(cron = "0 0/15 7-19 * * MON-FRI",zone = "BST")
 	   @Async
 	   public void auddis_cronJobSch() {
 		  
